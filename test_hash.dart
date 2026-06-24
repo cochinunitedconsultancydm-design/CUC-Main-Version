25a010999aaad1fb82a27759a243dfe21473a899
AVS <cochinunitedconsultancydm@gmail.com>
2026-06-24 17:52:10 +0530

    test: update hardcoded hash string in test_hash.dart

--- test_hash.dart
+++ test_hash.dart
@@ -2,5 +2,5 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
 void main() {
-  print(sha256.convert(utf8.encode('z2NYiTnUZNtlttRXnawbMA==:123456')));
+  print(sha256.convert(utf8.encode('_-hnrfubIKTb6iYQSiNV1w==:123456')));
 }

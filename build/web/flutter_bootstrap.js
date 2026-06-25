f0f8c8e91b7a8e3bb792d41c55adc10f6931b47c
AVS <cochinunitedconsultancydm@gmail.com>
2026-06-25 21:02:33 +0530

    chore: build web assets and initialize Flutter loader configuration

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2553689008" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "277276146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
